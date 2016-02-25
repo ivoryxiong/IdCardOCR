3a0bb8097e040b43dc671ea3c124ed90a5383126
ivoryxiong <ivoryxiong@ivoryxiongdeMacBook-Pro.local>
2016-02-25 22:24:51 +0800

    add project

--- IdCardOCR/ViewController.m
+++ IdCardOCR/ViewController.m
@@ -8,14 +8,30 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UIImagePickerControllerDelegate, UINavigationControllerDelegate>
+@property (nonatomic, strong) UIButton *autoRunnerBtn;
+@property (nonatomic, strong) UIButton *pickerBtn;
 
+@property (nonatomic, strong) UIImageView *imageView;
 @end
 
 @implementation ViewController
 
+- (void)loadView {
+    self.view = [[UIView alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    self.view.backgroundColor = [UIColor orangeColor];
+    
+    [self.view addSubview:self.pickerBtn];
+    self.pickerBtn.center = CGPointMake(CGRectGetMidX(self.view.bounds), 120);
+    
+    [self.view addSubview:self.imageView];
+    
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    self.title = @"ID Card OCR";
     // Do any additional setup after loading the view, typically from a nib.
 }
 
@@ -24,4 +40,50 @@
     // Dispose of any resources that can be recreated.
 }
 
+#pragma mark - actions
+- (void)showPicker {
+    UIImagePickerController *picker = [[UIImagePickerController alloc] init];
+    picker.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+    picker.delegate = self;
+    [self presentViewController:picker animated:YES completion:nil];
+}
+
+#pragma mark - UIImagePickerControllerDelegate
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<NSString *,id> *)info {
+
+    UIImage *image = info[UIImagePickerControllerOriginalImage];
+    if (image) {
+        self.imageView.image = image;
+        CGFloat height = self.imageView.bounds.size.width / image.size.width * image.size.height;
+        CGRect frame = self.imageView.frame;
+        frame.size.height = height;
+        self.imageView.frame = frame;
+    }
+
+    [picker dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker {
+    [picker dismissViewControllerAnimated:YES completion:nil];
+}
+#pragma mark - view getters
+- (UIButton *)pickerBtn {
+    if (_pickerBtn == nil) {
+        _pickerBtn = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, 120, 40)];
+        _pickerBtn.backgroundColor = [UIColor whiteColor];
+        [_pickerBtn setTitle:@"Pick Image" forState:UIControlStateNormal];
+        [_pickerBtn setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+        [_pickerBtn addTarget:self action:@selector(showPicker) forControlEvents:UIControlEventTouchUpInside];
+    }
+    
+    return _pickerBtn;
+}
+
+- (UIImageView *)imageView {
+    if (_imageView == nil) {
+        _imageView = [[UIImageView alloc] initWithFrame:CGRectMake(40, 200, self.view.bounds.size.width - 80, 120)];
+    }
+    
+    return _imageView;
+}
 @end
---
+++ Podfile
@@ -0,0 +1,17 @@
+# Uncomment this line to define a global platform for your project
+platform :ios, '7.0'
+# Uncomment this line if you're using Swift
+# use_frameworks!
+
+target 'IdCardOCR' do
+pod 'OpenCV', '~> 3.0.0'
+end
+
+target 'IdCardOCRTests' do
+
+end
+
+target 'IdCardOCRUITests' do
+
+end
+
